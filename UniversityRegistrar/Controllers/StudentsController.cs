c1530a46c1035889d32a75b38eca39515ba9dd92
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-08-04 15:57:38 -0700

    add views for details
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -19,43 +19,78 @@ namespace UniversityRegistrar.Controllers
 
     public ActionResult Index() 
     {
-      List<Student> model = _db.Students.ToList();
+      var model = _db.Students
+        .Include(student => student.Courses)
+        .ThenInclude(join => join.Course)
+        .ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Student student)
+    public ActionResult Create(Student student, int CourseId)
     {
       _db.Students.Add(student);
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId} );
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      var thisStudent = _db.Students
+      .Include(student => student.Courses)
+      .ThenInclude(join => join.Course)
+      .FirstOrDefault(students => students.StudentId == id);
+      //Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
       return View(thisStudent);
     }
 
     public ActionResult Edit (int id)
     {
       var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
       return View(thisStudent);
     }
 
     [HttpPost]
-    public ActionResult Edit(Student student)
+    public ActionResult Edit(Student student, int CourseId)
     {
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() {CourseId = CourseId, StudentId = student.StudentId});
+      }
       _db.Entry(student).State = EntityState.Modified; 
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
+    public ActionResult AddCourse (int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult AddCourse(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() {CourseId = CourseId, StudentId = student.StudentId});
+      }
+      _db.SaveChanges(); 
+      return RedirectToAction("Index");
+
+    }
     public ActionResult Delete(int id)
     {
       Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
@@ -64,9 +99,18 @@ namespace UniversityRegistrar.Controllers
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
-    {
+    { 
       Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
       _db.Students.Remove(thisStudent);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCourse (int joinId)
+    {
+      var joinEntry = _db.CourseStudent.FirstOrDefault(entry => entry.CourseStudentId == joinId);
+      _db.CourseStudent.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -5,9 +5,15 @@ namespace UniversityRegistrar.Models
 {
   public class Student
   {
+    public Student ()
+    {
+      this.Courses = new HashSet<CourseStudent>();
+    }
     public int StudentId {get;set;}
     public string FirstName {get;set;}
     public string LastName {get;set;}
     public DateTime DateEnrolled {get;set;}
+
+    public ICollection<CourseStudent> Courses {get;}
   }
 }
