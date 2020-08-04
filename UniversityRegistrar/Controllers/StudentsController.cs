108ba3c3554fb619798f712c26e3b145c228542a
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-08-04 11:36:47 -0700

    add index, create, details views to students
    Co-Authored-By: Teresa Rosinski <trosinski89@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -17,6 +17,43 @@ namespace UniversityRegistrar.Controllers
       _db = db;
     }
 
+    public ActionResult Index() 
+    {
+      List<Student> model = _db.Students.ToList();
+      return View(model);
+    }
 
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      return View(thisStudent);
+    }
+
+    public ActionResult Edit (int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      return View(thisStudent);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Student student)
+    {
+      _db.Entry(student).State = EntityState.Modified; 
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
